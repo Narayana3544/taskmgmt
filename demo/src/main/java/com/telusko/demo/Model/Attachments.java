afc4ab2c354536643fa1c125b32003415bbbe292
opentaps <lakshminarayana3544@gmail.com>
2025-06-17 16:22:25 +0530

    changed a table

--- demo/src/main/java/com/telusko/demo/Model/Attachments.java
+++ demo/src/main/java/com/telusko/demo/Model/Attachments.java
@@ -13,5 +13,30 @@ public class Attachments {
     @Id
     private int id;
     private int task_id;
+
+    public String getFilename_path() {
+        return filename_path;
+    }
+
+    public void setFilename_path(String filename_path) {
+        this.filename_path = filename_path;
+    }
+
+    public int getTask_id() {
+        return task_id;
+    }
+
+    public void setTask_id(int task_id) {
+        this.task_id = task_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private String filename_path;
 }

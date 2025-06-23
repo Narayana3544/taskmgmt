c90e55536bed05ee8e1f52132210387228d684ee
M V Suneel Reddy <msuneelreddy0@gmail.com>
2025-06-23 19:31:08 +0530

    added global api

--- demo/src/main/java/com/telusko/demo/controller/featurecontroller.java
+++ demo/src/main/java/com/telusko/demo/controller/featurecontroller.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
 public class featurecontroller {
 
--- demo/src/main/java/com/telusko/demo/controller/userRegisterController.java
+++ demo/src/main/java/com/telusko/demo/controller/userRegisterController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
 public class userRegisterController
 {
